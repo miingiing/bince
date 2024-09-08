9cf6d24bc3a1d0cc356eb8682a0dcad1731e282a
miingiing <96398691+miingiing@users.noreply.github.com>
2024-09-08 12:16:40 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 # Ubah direktori ke /usr/bin
 cd /usr/bin
 
 # Buat virtual environment Python
-python3 -m venv v 
-source v/bin/activate 
+python3 -m venv v
+source v/bin/activate
 
 # Install dependensi Python
 pip install requests 
@@ -14,11 +15,12 @@ pip install numpy==1.23.5
 pip install python-binance 
 pip install hmmlearn
 pip install matplotlib 
+pip install pandas
 
-# Jalankan tmate di background
+# Jalankan tmate di background dengan virtual environment
 echo "running tmate"
 tmate -F &
 
-# Tambahkan perintah tail -f untuk menjaga kontainer tetap hidup
+# Tambahkan perintah tail -f untuk menjaga skrip tetap berjalan
 echo "Docker tetap berjalan..."
-tail -f /dev/null
+while true; do sleep 3600; done
